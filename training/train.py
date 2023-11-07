3383b5d6bf3653ac2325773d2ecd2de95b147271
Zisimopoulou <zisimopouloua@protonmail.com>
2023-11-07 22:02:24 +0000

    Unfroze all layers in the loaded model. Implemented EarlyStopping and ModelCheckpoint callbacks for model training during fine-tuning.

--- training/train.py
+++ training/train.py
@@ -6,32 +6,42 @@ from utils.helper_functions import create_tensorboard_callback, plot_loss_curves
 from models.model_definition import create_model
 
 def train_model(model, train_data, test_data, class_names):
-    checkpoint_path = "model_checkpoints/cp.ckpt"
+    for layer in loaded_gs_model.layers:
+    layer.trainable = True
+
+    early_stopping = tf.keras.callbacks.EarlyStopping(monitor="val_loss",
+                                                  patience=3)
+
+    checkpoint_path = "fine_tune_checkpoints/"
     model_checkpoint = tf.keras.callbacks.ModelCheckpoint(checkpoint_path,
-                                                          monitor="val_accuracy",
-                                                          save_best_only=True,
-                                                          save_weights_only=True,
-                                                          verbose=0)
+                                                      save_best_only=True,
+                                                      monitor="val_loss")
+
+    reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(monitor="val_loss",
+                                                 factor=0.2,
+                                                 patience=2,
+                                                 verbose=1,
+                                                 min_lr=1e-7)
 
     mixed_precision.set_global_policy(policy="mixed_float16")
 
     tf.get_logger().setLevel('ERROR')
 
-    model.compile(loss="sparse_categorical_crossentropy",
-                  optimizer=tf.keras.optimizers.Adam(),
-                  metrics=["accuracy"])
+    loaded_gs_model.compile(loss="sparse_categorical_crossentropy",
+                            optimizer=tf.keras.optimizers.Adam(0.0001),
+                            metrics=["accuracy"])
 
-    history_101_food_classes_feature_extract = model.fit(train_data,
-                                                         epochs=3,
-                                                         steps_per_epoch=len(train_data),
-                                                         validation_data=test_data,
-                                                         validation_steps=int(0.15 * len(test_data)),
-                                                         callbacks=[create_tensorboard_callback("training_logs",
-                                                                                                "efficientnetb0_101_classes_all_data_feature_extract"),
-                                                                    model_checkpoint])
-
+    history_101_food_classes_all_data_fine_tune = loaded_gs_model.fit(train_data,
+                                                        epochs=100,
+                                                        steps_per_epoch=len(train_data),
+                                                        validation_data=test_data,
+                                                        validation_steps=int(0.15 * len(test_data)),
+                                                        callbacks=[create_tensorboard_callback("training_logs", "efficientb0_101_classes_all_data_fine_tuning"),
+                                                                   model_checkpoint,
+                                                                   early_stopping,
+                                                                   reduce_lr])
     results_created_model = model.evaluate(test_data)
 
-    save_dir = "07_efficientnetb0_feature_extract_model_mixed_precision"
+    save_dir = "07_efficientnetb0_feature_extract_model_mixed_precision_fine_tuning"
     model.save(save_dir)
 
